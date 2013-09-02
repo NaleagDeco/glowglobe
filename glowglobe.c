07ed543f05af6ca7fb70a6309574dd39caa8551c
Gaelan D'costa <gdcosta@gmail.com>
2013-09-02 17:36:18 -0400

    It lights up.

--- glowglobe.c
+++ glowglobe.c
@@ -1,14 +1,40 @@
 #include <wiringPi.h>
 #include <piGlow.h>
 
+//#include <signal.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+
+void exitOnHandle(int signum) {
+    exit(0);
+}
+
+
 int main(int argc, char** argv) {
     // Run 'gpio load i2c' externally.
 
     // Initialize PiGlow to have all LEDs off.
     piGlowSetup(1);
 
+    //Set up a handler so we can quit this program.
+    //struct sigaction handler;
+    //handler.
+
     // We should basically run forever until interrupted.
-    while(0) {
+    const int BRIGHTNESS = 100;
+    const int SLEEP_TIME_MS = 250;
+    const int TOTAL_RINGS = 5;
+
+    int active_ring = TOTAL_RINGS - 1;
+
+    while(true) {
+	piGlowRing(active_ring, 0);
+	active_ring = ++active_ring % TOTAL_RINGS;
+	piGlowRing(active_ring, BRIGHTNESS);
+
+	delay(SLEEP_TIME_MS);
     }
 
     return 0;
