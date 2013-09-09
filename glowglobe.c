4cf4feeeb94929c6329e0b0d8f2b70e125abcc6e
Gaelan D'costa <gdcosta@gmail.com>
2013-09-08 20:00:32 -0400

    Added signal handler, so we turn off the LEDs when we quit via Ctrl-C

--- glowglobe.c
+++ glowglobe.c
@@ -1,13 +1,22 @@
 #include <wiringPi.h>
 #include <piGlow.h>
 
-//#include <signal.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
+// Setup Constants
+const int BRIGHTNESS = 100;
+const int SLEEP_TIME_MS = 250;
+const int TOTAL_RINGS = 5;
 
 
 void exitOnHandle(int signum) {
+    // Turn off all PIGlow Leds.
+    int i;
+    for (i = 0; i < TOTAL_RINGS; ++i) {
+	piGlowRing(i, 0);
+    }
     exit(0);
 }
 
@@ -21,17 +30,19 @@ int main(int argc, char** argv) {
     //Set up a handler so we can quit this program.
     //struct sigaction handler;
     //handler.
+    struct sigaction quit_action;
+    quit_action.sa_handler = exitOnHandle;
+    quit_action.sa_flags = 0;
+
+    sigaction(SIGINT, &quit_action, NULL);
+    sigaction(SIGTERM, &quit_action, NULL);
 
     // We should basically run forever until interrupted.
-    const int BRIGHTNESS = 100;
-    const int SLEEP_TIME_MS = 250;
-    const int TOTAL_RINGS = 5;
-
     int active_ring = TOTAL_RINGS - 1;
 
     while(true) {
 	piGlowRing(active_ring, 0);
-	active_ring = ++active_ring % TOTAL_RINGS;
+	active_ring = (active_ring + 1) % TOTAL_RINGS;
 	piGlowRing(active_ring, BRIGHTNESS);
 
 	delay(SLEEP_TIME_MS);
